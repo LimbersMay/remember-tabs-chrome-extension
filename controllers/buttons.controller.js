18254c5c9ce7b93d566906590239d4e44249c425
limbersMay <limbermay1@gmail.com>
2022-10-31 15:15:26 -0600

    se llamó al controlador de los mensajes para mostrar el mensaje en pantalla

--- controllers/buttons.controller.js
+++ controllers/buttons.controller.js
@@ -1,3 +1,6 @@
+import { createMessageElement } from "../helpers/createMessageElement.js";
+import { handleMessage } from "../helpers/handleMessage.js";
+
 export const registerTabsEvent = async( tabAccess ) => {
 
     const tabsArray = [];
@@ -9,13 +12,20 @@ export const registerTabsEvent = async( tabAccess ) => {
     });
 
     tabAccess.setTabs(tabsArray);
+
+    // Mostramos el mensaje en pantalla
+    handleMessage( createMessageElement('Tabs saved', 'saved_tabs_msg'));
 };
 
 export const reOpenTabsEvent = async( storageAccess, tabAccess ) => {
     const tabs = await storageAccess.getTabs();
     tabAccess.openTabs(tabs);
+
+    handleMessage( createMessageElement('Tabs opened', 'restored_tabs_msg'));
 }
 
 export const removeTabsEvent = async( tabAccess ) => {
     tabAccess.setTabs([]);
+
+    handleMessage( createMessageElement('Tabs removed', 'remove_tabs_msg'));
 };
