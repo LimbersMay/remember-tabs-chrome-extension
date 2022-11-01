a447aaa655a8b2a705de82e35a6981516bf402d8
limbersMay <limbermay1@gmail.com>
2022-11-01 16:10:00 -0600

    se exportó los módulos de servicios de mozilla

--- services/index.js
+++ services/index.js
@@ -1,2 +1,4 @@
 export * from './chromeLocalStorage.service.js';
 export * from './chromeTab.service.js';
+export * from './firefoxLocalStorage.service.js';
+export * from './firefoxTab.service.js';
